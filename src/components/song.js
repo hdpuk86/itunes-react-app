94d444e37a46f374bf30a89ea5d97c4fc50d9284
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-09 21:20:32 +0000

    restructures list

--- src/components/song.js
+++ src/components/song.js
@@ -7,13 +7,15 @@ class Song extends React.Component {
     const songImage = songDetails['im:image'][2];
     return(
       <div className="song">
-        <p className="song-attr">{this.props.position + 1}. {songDetails['im:name'].label}</p>
-        <img className="song-attr" src={songImage.label} alt="single" height={songImage.attributes.height}/>
-        <section>
-          <p className="song-attr">{songDetails['im:name'].label}</p>
-          <p className="song-attr">{songDetails['im:artist'].label}</p>
-          <p className="song-attr">{songDetails.category.attributes.label}</p>
-          <p className="song-attr">{songDetails['im:price'].label}</p>
+        <p className="song-position">{this.props.position + 1}. {songDetails['im:name'].label}</p>
+        <section className="song-section">
+          <img className="song-attr" src={songImage.label} alt="single" height={songImage.attributes.height}/>
+          <div>
+            <p className="song-attr">Title:  {songDetails['im:name'].label}</p>
+            <p className="song-attr">Artist:  {songDetails['im:artist'].label}</p>
+            <p className="song-attr">Genre:  {songDetails.category.attributes.label}</p>
+            <p className="song-attr">Price:  {songDetails['im:price'].label}</p>
+          </div>
         </section>
       </div>
     )
