ebdf754b0e4b8c245f83f2ff39736598c3b203aa
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-09 19:05:56 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import TopSongsContainer from './containers/topSongsContainer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <TopSongsContainer />
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/topSongsContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TopSongsContainer extends React.Component{
+
+  render(){
+    return(
+      <div className="top-songs-container">
+        topSongsContainer
+      </div>
+    )
+  }
+
+}
+
+export default TopSongsContainer;
