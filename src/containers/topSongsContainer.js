8542db9d9713d3ab8f318df3788ef8ba104ce7d1
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-09 19:29:18 +0000

    adds api request

--- src/containers/topSongsContainer.js
+++ src/containers/topSongsContainer.js
@@ -3,6 +3,27 @@ import SongList from '../components/songList.js';
 
 class TopSongsContainer extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      topSongs: []
+    }
+  }
+
+  componentDidMount(){
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', () => {
+      if(request.status === 200){
+        const jsonString = request.responseText;
+        const data = JSON.parse(jsonString);
+        this.setState({topSongs: data.feed.entry});
+      }
+    })
+    request.send();
+  }
+
   render(){
     return(
       <div className="top-songs-container">
